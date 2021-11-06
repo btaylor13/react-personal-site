0a99c691517eb25d60652af1cbbc52877f353b4c
Bridgett Taylor <btaylor7224@gmail.com>
2021-11-05 17:23:39 -0700

    initial commit

---
+++ client/src/PorfolioContainer/Home/Profile.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <a href="https://github.com/btaylor13">
+              <i className="fa fa-github"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-paper-plane"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-linkedin-in"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
